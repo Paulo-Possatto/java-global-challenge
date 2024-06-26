ffc27a14faacf9c523d8cb009b619a1da6d5d82e
Paulo Possatto <paulo.possatto@outlook.com>
2024-06-26 17:49:40 +0200

    First commit

---
+++ src/main/java/com/globalpayments/challenge/JavaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.globalpayments.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChallengeApplication.class, args);
+	}
+
+}
